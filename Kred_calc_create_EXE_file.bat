f5f429bc7a2c6b3dad0418d4e1452f15da559bd3
Artem Liakhovskyi <54625091+LiaArtem@users.noreply.github.com>
2023-09-14 14:06:06 +0300

    update bat files

--- Kred_calc_create_EXE_file.bat
+++ Kred_calc_create_EXE_file.bat
@@ -1,10 +1,10 @@
 @ECHO OFF
+set PYTHONPATH="c:\Users\artem\AppData\Local\Programs\Python\Python311"
 
 rd /s /q build
 rd /s /q dist
 del /q main.spec
 
-cd %cd%
-"%cd%\venv\Scripts\pyinstaller.exe" --noconfirm --noconsole --onefile --icon=icon.ico main.py
+pyinstaller --noconfirm --noconsole --onefile --icon=icon.ico main.py
 
 pause
